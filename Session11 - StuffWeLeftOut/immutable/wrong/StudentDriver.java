80381e82ba1c05d281caa6f693e223040533f1bc
Havan <havanagrawal@gmail.com>
2017-06-30 21:03:44 +0530

    Add faulty examples for immutable classes

---
+++ Session11 - StuffWeLeftOut/immutable/wrong/StudentDriver.java
@@ -0,0 +1,47 @@
+/*
+ * This example shows a common mistake, i.e. leaking a mutable member variable
+ * by means of a getter.
+ */
+
+import java.util.*;
+
+final class Student {
+
+	List<Integer> marks;
+
+	public Student(int... marks) {
+
+		this.marks = new ArrayList<>();
+
+		for (int mark : marks) {
+			this.marks.add(mark);
+		}
+	}
+
+	public List<Integer> getMarks() {
+		return marks;
+	}
+
+	@Override
+	public String toString() {
+		return "Student: [" + marks.toString() + "]";
+	}
+}
+
+class StudentDriver {
+	public static void main(String[] args) {
+		Student s = new Student(98, 94, 99);
+
+		System.out.println(s);
+
+		// The getter returns a reference of the marks ArrayList
+		// Now leakedMarks points to the same instance as the member variable
+		// And you are able to modify the student's marks!
+		// This happens only when you return a reference of a mutable member variable
+		// And so this problem does not occur for Strings, Integers, etc
+		List<Integer> leakedMarks = s.getMarks();
+		leakedMarks.add(10);    // WHAT? NOOOOOOOOOOOOOOOOO
+
+		System.out.println(s);  // KT :(
+	}
+}
---
+++ Session11 - StuffWeLeftOut/immutable/wrong/VacationPricer.java
@@ -0,0 +1,57 @@
+/*
+ * This example shows a common mistake, using a mutable instance's reference
+ * directly in the constructor.
+ * This is similar to the Student example, but more subtle.
+ */
+
+// Note:  The price variable is extraneous in the example,
+// it is only to drive home the impact a fault immutable class can have.
+
+import java.util.*;
+import java.util.concurrent.*;
+
+final class Vacation {
+
+	Date from;
+	Date to;
+	double priceInDollars;
+
+	public Vacation(Date from, Date to) {
+		this.from = from;
+		this.to = to;
+
+		long differenceInMs = to.getTime() - from.getTime();
+
+		long noOfDays = TimeUnit.DAYS.convert(differenceInMs, TimeUnit.MILLISECONDS);
+
+		this.priceInDollars = noOfDays * 30;
+	}
+
+	public double getPriceInDollars() {
+		return priceInDollars;
+	}
+
+	@Override
+	public String toString() {
+		return "Vacation [from=" + from + ", to=" + to + ", price=" + priceInDollars + "]";
+	}
+}
+
+class VacationPricer {
+	public static void main(String[] args) {
+		// an example of an awful API
+		// Where year has to be 2017 - 1900
+		// and months start from 0, so 1 is actually Feb :|
+		Date from = new Date(117, 1, 6);
+		Date to = new Date(117, 1, 13);
+
+		Vacation v = new Vacation(from, to);
+		System.out.println(v);
+
+		// Extend vacation by 1 month!
+		to.setMonth(2);
+
+		// Vacation is extended, without any price change!
+		System.out.println(v);
+	}
+}
